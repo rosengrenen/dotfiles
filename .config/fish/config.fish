57c4b54909108615f3c055bba6d79f2ec403b5d0
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2021-09-14 01:41:52 +0200

    add git config, paru config, and more fish aliases/abbrs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,19 @@
 set -x PATH $PATH $HOME/.cargo/bin
 
+alias yay paru
+alias j z
+
+abbr --add gst git status
+abbr --add gaa git add --all
+abbr --add gds git diff --staged
+abbr --add gdh git diff HEAD
+abbr --add gl git log --oneline --graph
+
+abbr --add ls exa
+abbr --add la exa -al
+abbr --add l exa -l
+
+abbr --add bat cat
+
+zoxide init fish | source
 starship init fish | source
