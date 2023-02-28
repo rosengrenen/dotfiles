46673589b16236f2ca1b014caa2349d54a16a39b
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2023-02-28 19:17:23 +0100

    fix polybar script theme path

--- .config/polybar/scripts/window-switcher.sh
+++ .config/polybar/scripts/window-switcher.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rofi -no-config -no-lazy-grab -show window -sorting fzf -matching fuzzy -theme ~/.config/polybar/docky/scripts/rofi/launcher.rasi
+rofi -no-config -no-lazy-grab -show window -sorting fzf -matching fuzzy -theme ~/.config/polybar/scripts/rofi/launcher.rasi
