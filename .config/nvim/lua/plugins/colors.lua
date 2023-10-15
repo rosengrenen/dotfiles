2d4c34f1348e61ec6d6d5e6ffcb046917b0c4c6d
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2023-10-16 01:01:05 +0200

    formatting

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,15 +1,15 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  init = function()
-    vim.cmd.colorscheme "catppuccin"
-  end,
-  opts = {
-    flavour = "mocha",
-    integrations = {
-      cmp = true,
-      treesitter = true,
-    },
-  }
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    init = function()
+        vim.cmd.colorscheme "catppuccin"
+    end,
+    opts = {
+        flavour = "mocha",
+        integrations = {
+            cmp = true,
+            treesitter = true,
+        },
+    }
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,9 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  opts = {
-    auto_install = true,
-    highlight = {
-      enable = true,
-    },
-  }
+    'nvim-treesitter/nvim-treesitter',
+    opts = {
+        auto_install = true,
+        highlight = {
+            enable = true,
+        },
+    }
 }
