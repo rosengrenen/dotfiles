a76e2b58f053bcb7599d7a10ebf5dd0719dfd3dc
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2023-10-16 10:38:09 +0200

    fix treesitter setup and add some keybinds to select tree nodes

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.4",
+    lazy = false,
     dependencies = {
         "nvim-lua/plenary.nvim",
         "folke/which-key.nvim",
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,30 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        "folke/which-key.nvim",
+    },
+    init = function()
+        local wk = require("which-key")
+        wk.register({
+            ["<leader>t"] = { name = "Select syntax tree" }
+        })
+    end,
     opts = {
         auto_install = true,
         highlight = {
             enable = true,
         },
-    }
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<leader>tt",
+                node_incremental = "<leader>tk",
+                scope_incremental = "<leader>tl",
+                node_decremental = "<leader>tj",
+            },
+        },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end
 }
