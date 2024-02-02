11604bc990fe998839fcbeef17ce917e9673c71a
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2024-02-02 19:20:14 +0100

    add comment.nvim

---
+++ .config/nvim/lua/plugins/comments.lua
@@ -0,0 +1,7 @@
+return {
+    {
+        "numToStr/Comment.nvim",
+        opts = {},
+    },
+    "JoosepAlviste/nvim-ts-context-commentstring", -- Detect embedded language and comment correctly
+}
