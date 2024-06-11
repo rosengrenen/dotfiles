b6eae31e1fae9c41a0f1b140d2075bdf54ec9252
Rasmus Rosengren <rasmus.rosengren@protonmail.com>
2024-06-11 22:25:02 +0200

    fix show scratch pad

--- .config/runst/scripts/show-scratchpad.sh
+++ .config/runst/scripts/show-scratchpad.sh
@@ -4,16 +4,16 @@ lockdir=/tmp/runst-scratchpad-show.lock
 if mkdir "$lockdir"
 then
     window_id=$(xdotool search --name "RuneLite - ")
-
+    output=$(i3-msg -t get_tree | jq ".. | select(.window? == $window_id) | .output")
     if [[ "$output" == '"__i3"' ]]; then
-        i3-msg [id=$window_id] scratchpad show
-        i3-msg [id=$window_id] focus
+        i3-msg "[id=$window_id]" scratchpad show
+        i3-msg "[id=$window_id]" focus
     else
         current_workspace_id=$(i3-msg -t get_workspaces | jq '.[] | select(.focused == true) | .id')
         find_window_in_workspace_result=$(i3-msg -t get_tree | jq ".. | select(.id? == $current_workspace_id) | .. | select(.window? == $window_id) | .id")
         if [ -z $find_window_in_workspace_result ]; then
-            i3-msg [id=$window_id] move container to workspace current
-            i3-msg [id=$window_id] focus
+            i3-msg "[id=$window_id]" move container to workspace current
+            i3-msg "[id=$window_id]" focus
         fi
     fi
 
